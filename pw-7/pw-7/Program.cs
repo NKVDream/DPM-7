9bac743e7364d0fe952bf41219cfc3f4b08a8704
NKVDream <Kmv235@yandex.ru>
2025-03-23 03:41:29 -0700

    first task is done

---
+++ pw-7/pw-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "shy", "shock", "shreder" };
+        string prefix = "";
+        int i = 0;
+        while (true)
+        {
+            char a = words[0][i];
+            foreach (var word in words)
+            {
+                if (i >= word.Length || word[i] != a)
+                {
+                    goto END;
+                }
+            }
+            prefix += a;
+            i++;
+        }
+
+        END: Console.WriteLine($"The longest prefix is: {prefix}");
+    }
+}
