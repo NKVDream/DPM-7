657a71d1b2d014f629e6bed792c5947eb1200c6c
NKVDream <Kmv235@yandex.ru>
2025-03-23 21:25:07 -0700

    second task is done

---
+++ dpm-7-2/dpm-7-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Write down any word: ");
+        string word = Console.ReadLine();
+        Console.WriteLine();
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == word.ToUpper()[i])
+            {
+                Console.Write(word.ToLower()[i]);
+            }
+            else if (word[i] == word.ToLower()[i])
+            {
+                Console.Write(word.ToUpper()[i]);
+            }
+        }
+    }
+}
