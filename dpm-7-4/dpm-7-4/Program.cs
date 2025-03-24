5d91ff6c560c3429ee81b6569bed77f0bb1442cf
NKVDream <Kmv235@yandex.ru>
2025-03-23 22:35:53 -0700

    fourth task is done

---
+++ dpm-7-4/dpm-7-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("write down any number: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine(Chek(num1));
+        Console.WriteLine("write down any number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine(Chek(num2));
+    }
+    static bool Chek(int n)
+    {
+        {
+            for (int i = 1; i <= n; i *= 2)
+            {
+                if (i == n) return true;
+            }
+            return false;
+        }
+    }
+}
