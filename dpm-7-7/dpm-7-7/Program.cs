bd602ab126dcc6ede72eebbc34bd959b641f63e8
NKVDream <Kmv235@yandex.ru>
2025-03-25 05:24:15 -0700

    seventh task is done

---
+++ dpm-7-7/dpm-7-7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int[] gas = new int[5];
+        for (int i = 0; i < gas.Length; i++)
+        {
+            gas[i] = random.Next(1,5);
+        }
+        int[] cost = new int[5];
+        for (int i = 0;i < cost.Length; i++)
+        {
+            cost[i] = random.Next(1,5);
+        }
+        Console.WriteLine(Canister(gas, cost));
+    }
+    static int Canister(int[] gas, int[] cost)
+    {
+        int n = gas.Length;
+        int totalTank = 0;
+        int currentTank = 0;
+        int startingStation = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            totalTank += gas[i] - cost[i];
+            currentTank += gas[i] - cost[i];
+
+            if (currentTank < 0)
+            {
+                startingStation = i + 1;
+                currentTank = 0;
+            }
+        }
+
+        return totalTank >= 0 ? startingStation : -1;
+    }
+}
