69af697df8041001328b463816b4a2d6af27d407
NKVDream <Kmv235@yandex.ru>
2025-03-25 06:06:07 -0700

    eighth task is done

---
+++ dpm-7-8/dpm-7-8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("write down some numbers: ");
+        string N = Console.ReadLine();
+        Console.WriteLine(Func(N));
+    }
+    static string Func( string n)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (char i in n)
+        {
+            int count = int.Parse(i.ToString());
+            result.Append(i, count);
+        }
+
+        return result.ToString();
+    }
+}
